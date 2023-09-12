79746818276fe7bff9d42538ad4dd088d38f64d1
Jaswant Singh <jaswant.singh@elxoinc.com>
2023-09-12 11:58:52 +0530

    Add post section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./assets/css/style.css"
 import Profile from "./pages/Profile";
+import AddPosts from "./components/AddPosts";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/post-add" element={<AddPosts />} />
       </Routes>
     </div>
   );
--- src/components/AddPosts.js
+++ src/components/AddPosts.js
@@ -1,31 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
-const Posts = () => {
+const AddPosts = () => {
+  const [previewImages, setPreviewImages] = useState([]);
+  const [formFields, setFormFields] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+  const inputHandler = (e) => {
+    setFormFields({ ...formFields, [e.target.name]: e.target.value });
+  };
+  const imageUploadHandler = (e) => {
+    let files = e.target.files;
+    let images = [];
+    for (let a = 0; a < files?.length; a++) {
+      images.push(URL.createObjectURL(e.target.files[a]));
+    }
+    setPreviewImages(images);
+  };
+
+  const removeImageHandler = (index) => {
+    debugger
+    previewImages.splice(index, 1);
+  };
+
   return (
     <div className="add-posts-container">
       <div className="add-posts-head-box">
-        <h3>Add New Post</h3>
+        <div className="add-posts-title">
+          <h3>Add New Post</h3>
+        </div>
       </div>
 
       <div className="add-posts-form-box">
         <div className="form-box">
           <form>
-            <div className="input-box">
-              <label htmlFor="title">Title</label>
-              <input type="text" id="title" className="form-control" />
+            <div className="input-box ">
+              <label htmlFor="title ">Title</label>
+              <input
+                type="text"
+                id="title"
+                name="content"
+                value={formFields.title}
+                className="form-control"
+                onChange={inputHandler}
+              />
             </div>
-            <div className="content">
+            <div className="content input-box ">
               <label htmlFor="title">Content</label>
               <textarea
                 cols={3}
                 type="text"
                 id="content"
                 className="form-control"
+                name="content"
+                value={formFields.content}
+                onChange={inputHandler}
               />
             </div>
             <div className="input-box">
               <label htmlFor="title">Image Upload</label>
-              <input type="file" id="title" className="form-control" />
+              <input
+                type="file"
+                id="title"
+                className="form-control"
+                accept="image/*"
+                multiple={true}
+                onChange={imageUploadHandler}
+              />
+            </div>
+            <div className="input-box">
+              <label htmlFor="title">Preview Images</label>
+
+              <div className="preview-upload-posts">
+                {previewImages?.map((previewImage, index) => (
+                  <div className="preview-img-box" key={index}>
+                    <img src={previewImage} />
+                    <CloseIcon
+                      className="remove-img-icon"
+                      onClick={() => {
+                        removeImageHandler(index);
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           </form>
           <div className="submit-btn-box">
@@ -37,4 +97,4 @@ const Posts = () => {
   );
 };
 
-export default Posts;
+export default AddPosts;
